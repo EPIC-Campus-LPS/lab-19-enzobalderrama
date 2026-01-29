dd183e1bea8e2e33401523587f487560bbe78c8a
enzobalderrama <76264578+enzobalderrama@users.noreply.github.com>
2026-01-29 00:11:43 -0700

    Update DoublyLinkedList.java

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -2,22 +2,29 @@ public class DoublyLinkedList<E> implements List<E>{
     private Node<E> head;
 
 
-    // Adding element by going through to see empty node and adding there
+    /**
+     * @param element element to add to the list, make sure head is not null
+     */
     public void add(E element) {
         if(head == null){
             head = new Node<>(element);
             return;
         }
-        Node<E> node = head;
+        Node node = head;
         while (node.getNextNode() != null){
-            node = node.getNextNode();
+            node = (node.getNextNode());
         }
-        Node newNode = new Node(element);
+        Node newnode = new Node<>(element);
         //balancing out the nodes and connecting them
-        node.setNextNode(newNode);
-        newNode.setNextNode(node);
+        node.setNextNode(newnode);
     }
 
+    /**
+     * @param i       index of the element
+     * @param element element to add to the list
+     * @throws IndexOutOfBoundsException
+     * added at specific place using count vs. i
+     */
     public void add(int i, E element) throws IndexOutOfBoundsException{
         if(head == null){
             head = new Node<>(element);
@@ -25,10 +32,9 @@ public class DoublyLinkedList<E> implements List<E>{
         }
         Node<E> node = head;
         int count = 0;
-        Node newNode = null;
         while (node.getNextNode() != null){
             if (count == i){
-                newNode = new Node(element);
+                node.setValue(element);
                 break;
             }
             count++;
@@ -37,15 +43,18 @@ public class DoublyLinkedList<E> implements List<E>{
         if (i > count){
             throw new IndexOutOfBoundsException("Index " + i + " is out of bounds");
         }
-        node.setNextNode(newNode);
-        newNode.setNextNode(node);
+
     }
 
+    /**
+     * Removes the last one of the nodes, checks until one is null
+     */
     public void remove() {
         if(head == null){
             return;
         }
         Node<E> node = head;
+        head = null;
         while (node.getNextNode() != null){
             node.setValue(null);
             node = node.getNextNode();
@@ -87,25 +96,20 @@ public class DoublyLinkedList<E> implements List<E>{
      * @param i index of the element
      * @return the element
      */
-    public Node<E> get(int i) throws IndexOutOfBoundsException{
-        if(head == null){
-            return head;
+    public E get(int i) throws IndexOutOfBoundsException{
+        if (i >= size() || i < 0){
+            throw new IndexOutOfBoundsException("Index " + i + " is out of bounds");
         }
         Node<E> node = head;
-        Node newNode = null;
         int count = 0;
-        while (node.getNextNode() != null){
-            if (count == i){
-                newNode = new Node(node.getValue());
-                break;
+        while (node != null) {
+            if (count == i) {
+                return node.getValue();
             }
             count++;
             node = node.getNextNode();
         }
-        if (i > count){
-            throw new IndexOutOfBoundsException("Index " + i + " is out of bounds");
-        }
-        return newNode;
+        return null;
     }
 
     /**
@@ -115,45 +119,62 @@ public class DoublyLinkedList<E> implements List<E>{
      * @param element new value of the element
      */
     public void set(int i, E element) throws IndexOutOfBoundsException{
-        if (size() < i) {
+        if (size() < i || i < 0) {
             throw new IndexOutOfBoundsException("Index " + i + " is out of bounds");
         }
-        if(head == null){
-            head = new Node<>(element);
-            return;
-        }
         Node<E> node = head;
         int count = 0;
-        while (node.getNextNode() != null) {
+        while (node != null) {
             if (count == i) {
-                node = new Node(element);
-                break;
+                node.setValue(element);
+                return;
             }
-            count++;
             node = node.getNextNode();
+            count++;
         }
     }
+
+    /**
+     *  Find and return the size of the node by iterating through it
+     *  @return count
+     */
     public int size() {
+        if (head == null){
+            return 0;
+        }
         Node<E> node = head;
-        int count = 0;
+        int count = 1;
         while (node.getNextNode() != null){
             count++;
             node = node.getNextNode();
         }
         return count;
     }
+
+    /**
+     * Check if list and head is empty
+     * @return true if empty false if not
+     */
     public boolean isEmpty() {
         if(head == null){
             return true;
         }
         return false;
     }
+
+    /**
+     * @return String of nodes
+     */
     public String toString() {
         String str = "[";
         for (int i = 0; i < size(); i++) {
-            str += get(i) + ", ";
+            if (i > 0){
+                str += ", ";
+            }
+            str += (get(i));
         }
         str += "]";
+        str = String.valueOf(str);
         return str;
     }
 }
